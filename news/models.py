55345205e0ce4a19f630f8a6768a1f9fda72ce82
Bruno <bruno.odwuor19@gmail.com>
2023-12-06 15:59:46 +0300

    first version

---
+++ news/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+class News(models.Model):
+    headline = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateField()
+    def __str__(self):
+        return self.headline
